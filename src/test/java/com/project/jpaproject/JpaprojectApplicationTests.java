5724e13cf3ccbbb3e8b79500045cff9a1e2c9ecf
marciofiladelfo <marcio.kabana@gmail.com>
2021-04-14 17:01:03 -0300

    Project created

---
+++ src/test/java/com/project/jpaproject/JpaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jpaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
