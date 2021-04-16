adb5005555175a10a2f9e9ddb23778a0ac237964
marciofiladelfo <marcio.kabana@gmail.com>
2021-04-16 10:59:17 -0300

    OrderStatus enum

---
+++ src/main/java/com/project/jpaproject/entities/enums/OrderStatus.java
@@ -0,0 +1,29 @@
+package com.project.jpaproject.entities.enums;
+
+public enum OrderStatus {
+	
+	WAITING_PAYMENT(1),
+	PAID(2),
+	SHIPPED(3),
+	DELIVERED(4),
+	CANCELED(5);
+	
+	private int code;
+	
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	public static OrderStatus valueOf(int code) {
+		for(OrderStatus element : OrderStatus.values()) {
+			if(element.getCode() == code) {
+				return element;
+			}
+		}
+		throw new IllegalArgumentException("Invalid order status code");
+	}
+}
